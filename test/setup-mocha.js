a9c845dbc5578fe7e5c8f11dc6f64e908f0f800c
Neogost <kevin.desmay@gmail.com>
2023-12-27 22:54:14 +0100

    feature: mise a jour des fichier de configuration du workspace

--- test/setup-mocha.js
+++ test/setup-mocha.js
@@ -1,9 +1,13 @@
 //inject mocha globally to allow custom interface refer without direct import - bypass bundle issue
-global._ = require('lodash');
-global.mocha = require('mocha');
-global.chai = require('chai');
-global.sinon = require('sinon');
-global.chai.use(require('sinon-chai'));
+global._ = require("lodash");
+global.mocha = require("mocha");
+global.chai = require("chai");
+global.sinon = require("sinon");
+global.chai.use(require("sinon-chai"));
+module.exports = {
+  require: ["screeps-mock"],
+  timeout: 10000
+};
 
 // Override ts-node compiler options
-process.env.TS_NODE_PROJECT = 'tsconfig.test.json'
+process.env.TS_NODE_PROJECT = "tsconfig.test.json";
